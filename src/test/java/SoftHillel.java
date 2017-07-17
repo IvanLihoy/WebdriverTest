5061634956c72d5db111425970b6c7ee7c43ab26
IvanLihoy <Lihoy92ivan@gmail.com>
2017-07-18 00:41:17 +0300

    commit

--- src/test/java/SoftHillel.java
+++ src/test/java/SoftHillel.java
@@ -1,25 +1,18 @@
-import com.gargoylesoftware.htmlunit.ElementNotFoundException;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.*;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Created by hillel on 14.07.17.
- */
 public class SoftHillel {
 
     WebDriver driver;
 
-    @BeforeMethod
+    @BeforeTest
     void setUP(){
         driver = new ChromeDriver();
         driver.get("http://soft.it-hillel.com.ua:3000/");
@@ -27,27 +20,37 @@ public class SoftHillel {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
+    private void sleep(int time) {
+        try {
+            Thread.sleep(time * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Test
     void createUser() throws InterruptedException {
         WebElement plusButton = driver.findElement(By.cssSelector(".mdi-content-add"));
         plusButton.click();
-        Thread.sleep(2000);
+        sleep(2);
         WebElement nameField = driver.findElement(By.cssSelector("#icon_prefix"));
         nameField.clear();
         nameField.sendKeys("Ivan");
         WebElement phoneField = driver.findElement(By.cssSelector("#icon_telephone"));
         phoneField.clear();
         phoneField.sendKeys("111-111-1111");
-        WebElement dropdown = driver.findElement(By.cssSelector(".select-dropdown"));
-        dropdown.click();
-//        Select select = new Select(driver.findElement(By.cssSelector(".initialized")));
-//        select.selectByVisibleText("Administrator");
-        driver.findElement(By.xpath(".//*[@id='select-options-dc96ea45-38fd-2d54-b41c-f8ef088d3f9d']/li[1]/span")).click();
+        driver.findElement(By.xpath("html/body/div[1]/div[2]/a")).click();
+        Actions action = new Actions(driver);
+        WebElement elem = driver.findElement(By.xpath("html/body/div[1]/div[1]/div"));
+        action.moveToElement(elem);
+        action.perform();
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0,450)", "");
+        sleep(10);
     }
 
-    @AfterMethod
-    void tearDown(ITestResult testResult){
-        //System.out.println(testResult.getMethod().getDescription());
-        driver.close();
+    @AfterTest
+    void tearDown(){
+        driver.quit();
     }
 }
